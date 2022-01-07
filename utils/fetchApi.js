89cb55a469a19cd6710fdde1e2b4871d7a260fc3
derickson.loss <derickson.loss@paylivre.com>
2022-01-07 18:22:57 -0300

    implements the api fetching utility function

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "9756b913femshb1a7b319240560bp1ed080jsn36bc933af8b5",
+    },
+  });
+};

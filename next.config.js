0f594e19f18e7b472cebab832824399970ad889f
derickson.loss <derickson.loss@paylivre.com>
2022-01-16 16:23:26 -0300

    adds svg import handler in  next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
-  }
-}
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
+  },
+  module: {
+    rules: [
+      {
+        type: 'asset',
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+};
